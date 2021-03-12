77716cfff1af3b83516075f5408647a472c2c869
Ilya Grishkov <ilyagrishkov@icloud.com>
2021-03-12 20:13:56 +0200

    Working basic parsing

---
+++ src/Jq/JParser.hs
@@ -0,0 +1,45 @@
+module Jq.JParser where
+
+import Parsing.Parsing
+import Jq.Json
+
+parseNull :: Parser JSON
+parseNull = JNull <$ string "null"
+
+parseBool :: Parser JSON
+parseBool = jsonTrue <|> jsonFalse
+  where
+     jsonTrue = JBool True <$ string "true"
+     jsonFalse = JBool False <$ string "false"
+
+parseNum :: Parser JSON
+parseNum = JNum <$> parseDouble
+    
+parseDouble :: Parser Double
+parseDouble = createDouble 
+    <$> integer
+    <*> ((read <$> (('0':) <$> ((:) <$> char '.' <*> (show <$> integer)))) <|> pure 0)
+    <*> ((char 'e' <|> char 'E') *> integer <|> pure 0)
+
+createDouble :: Int -> Double -> Int -> Double
+createDouble real float expo = (fromIntegral real + float) * (10 ^^ expo)
+   
+
+parseString :: Parser JSON
+parseString = JString <$> (char '\"' *> identifier <* char '\"')
+
+sepBy :: Parser a -> Parser b -> Parser [b]
+sepBy sep element = (:) <$> element <*> many (sep *> element) <|> pure []
+
+parseArray :: Parser JSON
+parseArray = JArray <$> (char '[' *> space *> elems <* space <* char ']')
+  where
+    elems = sepBy (space *> char ',' <* space) parseJSON
+
+parseObject :: Parser JSON
+parseObject = JObject <$> (char '{' *> space *> sepBy (space *> char ',' <* space) keyValue <* space <* char '}')
+  where
+    keyValue = (\key _ val -> (key, val)) <$> (char '\"' *> identifier <* char '\"') <*> (space *> char ':' <* space) <*> parseJSON
+
+parseJSON :: Parser JSON
+parseJSON = token (parseNull <|> parseBool <|> parseNum <|> parseString <|> parseArray <|> parseObject)
---
+++ src/Jq/Json.hs
@@ -0,0 +1,18 @@
+module Jq.Json where
+
+data JSON =
+    JNull | JNum Double | JBool Bool | JString String | JArray [JSON] | JObject[(String, JSON)]
+    deriving (Eq)
+    
+instance Show JSON where
+  show JNull = "null"
+  show (JNum n) = if n == fromInteger (round n) then show (round n :: Integer) else show n
+  show (JBool b) = show b
+  show (JString s) = show s
+  show (JArray a) = show a
+  show (JObject o) = "{" ++ formatObject o ++ "}"
+
+formatObject :: Show a => [(String, a)] -> String
+formatObject [] = ""
+formatObject [(k, v)] = k ++ ":" ++ show v
+formatObject ((k, v):xs) = k ++ ":" ++ show v ++ "," ++ formatObject xs
