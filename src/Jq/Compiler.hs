39e1ab7bb33ea0741840b3e325459559fb11bd96
Ilya Grishkov <ilyagrishkov@icloud.com>
2021-03-19 19:06:29 +0200

    pipe working

--- src/Jq/Compiler.hs
+++ src/Jq/Compiler.hs
@@ -14,7 +14,7 @@ compile (OptionalObjectIdentifierIndex i) = compileOptionalObjectIdentifierIndex
 compile (GenericObjectIndex i) = compileObjectIdentifierIndex i
 compile (ArrayIndex i) = compileArrayIndex i
 compile (Slice s) = uncurry compileSlice s
-compile (Comma f) = uncurry compileComma f 
+compile (Comma f) = uncurry compileComma f
 compile (Pipe f) = uncurry compilePipe f
 
 compileValueIterator :: JProgram [JSON]
@@ -26,14 +26,14 @@ compileValueIterator (JString s) = error ("Cannot iterate over string (" ++ s ++
 compileValueIterator (JBool b) = error ("Cannot iterate over boolean (" ++ show b ++ ")")
 
 compileObjectIdentifierIndex :: String -> JProgram [JSON]
-compileObjectIdentifierIndex i (JObject o) = 
+compileObjectIdentifierIndex i (JObject o) =
   case map snd (filter (\(a, _) -> a == i) o) of
     [] -> Left "couldn't select anything"
     arr -> Right arr
 compileObjectIdentifierIndex _ _ = Left "couldn't select anything"
 
 compileOptionalObjectIdentifierIndex :: String -> JProgram [JSON]
-compileOptionalObjectIdentifierIndex i (JObject o) = 
+compileOptionalObjectIdentifierIndex i (JObject o) =
   case map snd (filter (\(a, _) -> a == i) o) of
     [] -> Right [JNull]
     arr -> Right arr
@@ -51,7 +51,12 @@ compileComma :: Filter -> Filter -> JProgram [JSON]
 compileComma f1 f2 inp = (++) <$> compile f1 inp <*> compile f2 inp
 
 compilePipe :: Filter -> Filter -> JProgram [JSON]
-compilePipe f1 f2 inp = undefined
- 
+compilePipe f1 f2 inp = compile f1 inp >>= compileSecondPipe f2
+      
+compileSecondPipe :: Filter -> [JSON] -> Either String [JSON]
+compileSecondPipe _ [] = Right [JNull]
+compileSecondPipe f [x] = compile f x
+compileSecondPipe f (x:xs) = (++) <$> compile f x <*> compileSecondPipe f xs
+      
 run :: JProgram [JSON] -> JSON -> Either String [JSON]
 run p j = p j
